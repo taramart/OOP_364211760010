09c011d8ba2349591fbdb958fb355c10e6589cd9
few <yuttana.d@rmutsvmail.com>
2022-08-24 16:12:51 +0700

    test

---
+++ Assignment/Assignment1_3.py
@@ -0,0 +1,19 @@
+print("Name: {Taramart Kaewmanee}")
+print("SID: {364211760015}")
+print("Group: {MIT221}")
+print("********************")
+def getPID():
+ pid = input ('Enter your pid:') #str
+ lpid = [int(x) for x in pid]
+ return lpid
+
+def getFortune(var):
+    if var % 2 == 0:
+      print('Your Fortune Is Good')
+    else:
+      print('Your Fortune Is Very Good')
+
+PID = getPID()
+print(f'เลขบัตรประชาชน: {sum(PID)}')
+print(f'ทำนายดวงชะตา: {getFortune(sum(PID))}')
+print("********************")
